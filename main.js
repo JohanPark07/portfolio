20087569e3b54d43564d019a97e1bd8fd522b3db
Johan Park <65893683+JohanPark07@users.noreply.github.com>
2022-04-04 17:13:47 +0900

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -11,18 +11,6 @@ document.addEventListener("scroll", () => {
   }
 });
 
-// Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector(".navbar__menu");
-navbarMenu.addEventListener("click", (event) => {
-  console.log(event.target.dataset.link);
-  const target = event.target;
-  const link = target.dataset.link;
-  if (link === null) {
-    return;
-  }
-  scrollIntoView(link);
-});
-
 // Handle click on "contact me" button on home
 
 const homeContactBtn = document.querySelector(".home__contact");
@@ -45,6 +33,25 @@ document.addEventListener("scroll", () => {
   } else {
     arrowUp.classList.remove("visible");
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  console.log(event.target.dataset.link);
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === null) {
+    return;
+  }
+  navbarMenu.classList.remove("open");
+  scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
+navbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
 });
 
 // Handle click on the "arrow up" button
